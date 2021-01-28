7c9636f9b1d71663ed9ccd796a7d1be2e3053c92
mineshmane <mineshmane94@gmail.com>
2021-01-28 12:43:05 +0530

    Added:pligins for class

--- app/components/register.jsx
+++ app/components/register.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
-export default class Register extends React.Component{
-    render(){
-        return(
-            <div>
-                resgitarion component
+export default class Register extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            first: ''
+        }
+    }
+
+    handle = () => {
+        console.log("not ded");
+        this.setState({ first: "added" },()=>{
+            console.log(this.state.first);
+        })
+    }
+    render() {
+
+
+        return (
+            <div className="main">
+                <div onClick={this.handle}>
+                    resgitarion component
+
+                </div>
             </div>
         )
     }
--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,78 @@
 var path = require('path');
+const webpack = require('webpack');
 
-var HtmlWebpackPlugin =  require('html-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const port = process.env.PORT || 3000;
 
 module.exports = {
 
-    entry : './app/index.js',
+    entry: './app/index.js',
 
-    resolve:{
-        extensions:['*','.jsx','.js']
+    resolve: {
+        alias: {
+            "react-dom": "@hot-loader/react-dom",
+          },
+        extensions: ['*', '.jsx', '.js']
     },
-    output : {
+    output: {
 
-        path : path.resolve(__dirname , 'dist'),
+        path: path.resolve(__dirname, 'dist'),
 
         filename: 'index_bundle.js'
 
     },
+    devtool: 'inline-source-map',
 
-    module : {
+    module: {
 
-        rules : [
+        rules: [
 
-            {test : /\.(jsx|js)$/, use:['babel-loader'],exclude:/node_modules/},
-          
+            { test: /\.(jsx|js)$/, use: ['babel-loader'], exclude: /node_modules/ },
 
-            {test : /\.css$/,exclude:/node_modules/, use:['style-loader', 'css-loader',]}
+
+            { test: /\.css$/, exclude: /node_modules/, use: ['style-loader', 'css-loader',] },
+            // Second Rule
+            // {
+            //     test: /\.css$/,
+            //     use: [
+            //         {
+            //             loader: 'style-loader'
+            //         },
+            //         {
+            //             loader: 'css-loader',
+            //             options: {
+            //                 modules: true,
+            //                 localsConvention: 'camelCase',
+            //                 sourceMap: true
+            //             }
+            //         }
+            //     ]
+            // }
 
         ]
 
     },
 
-    mode:'development',
+    mode: 'development',
 
-    plugins : [
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
 
-        new HtmlWebpackPlugin ({
+        new HtmlWebpackPlugin({
 
-            template : 'app/index.html'
+            template: 'app/index.html'
 
         })
 
-    ]
+    ],
 
+    devServer: {
+        host: 'localhost',
+        port: port,
+        historyApiFallback: true,
+        open: true,
+        hot: true
+      }
 
 
 
